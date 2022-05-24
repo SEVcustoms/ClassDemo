e8e5c005d0cce3c1ae280bf15844d19e0c7234c5
SEVcustoms <sevbrain@gmail.com>
2022-05-24 20:07:59 +0300

    add some test

--- src/main/java/org/SEVcustoms/Main.java
+++ src/main/java/org/SEVcustoms/Main.java
@@ -4,17 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /**
-        MuthDemo muthDemo1 = new MuthDemo(10, 2,"text1");
-        MuthDemo muthDemo2 = new MuthDemo(5, 3, "text2");
 
-        muthDemo1.doSquareUp();
-        muthDemo2.doString();
-
-        System.out.println("Число X = " + muthDemo1.getX());
-        System.out.println("Число Y = " + muthDemo2.getY());
-        System.out.println(muthDemo1.getText() + muthDemo2.getText());
-         **/
         DialogClass dialog = new DialogClass();
         dialog.dialogMethod();
     }
